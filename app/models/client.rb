b8c7158e1b17bacf924774b1019081ac91818f16
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-04-07 19:02:37 -0500

    bcrypt install and has_secure password added to client and employee

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,7 @@
 class Client < ApplicationRecord
     has_many :appointments
     has_many :empolyees, through::appointments
-    
+
+    has_secure_password
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,6 @@ class Employee < ApplicationRecord
     has_many :notes
     has_many :appointments
     has_many :clients, through::appointments
+
+    has_secure_password
 end
