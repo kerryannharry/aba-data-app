1561b4ee60c3d88f9e4c64da1a1c2e2e810b2545
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-04-14 09:49:07 -0500

    sessions create method .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController
-   
+   def new
+   end
+
+   def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        # redirect_to 
+    else
+        flash[:error] = "Invalid email or password."
+        render :new
+   end 
 end
