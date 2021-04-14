30ce5f0dd61fda77c5925b2474d24de4cf1f9f3f
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-04-13 19:12:59 -0500

    routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see 
-  resource :registration
-  
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :employees, except: [:new, :create]
+  resources :clients, except: [:new, :create]
+
 end
