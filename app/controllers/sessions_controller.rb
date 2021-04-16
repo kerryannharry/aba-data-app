e297e6f70714bd5a6ccaf6db45ca7d20e891dfdc
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-04-15 19:13:50 -0500

    destory method added to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,20 @@
 class SessionsController < ApplicationController
    def new
+    user = User.create(email: params[:email])
    end
 
    def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        # redirect_to 
+        # redirect_to home, notice: "Logged in successfully!"
     else
         flash[:error] = "Invalid email or password."
         render :new
+   end 
+
+   def destroy
+    session[:user_id] = nil
+    redirect_to login, notice: "Logged out!"
    end 
 end
