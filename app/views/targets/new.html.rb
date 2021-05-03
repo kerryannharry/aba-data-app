767e8a254e592fc30f19b224bddd5896c9c02612
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-05-02 21:42:33 -0500

    Target page updates

--- app/views/targets/new.html.rb
+++ app/views/targets/new.html.rb
@@ -1,4 +1,4 @@
-<h1> New Target: </h1>
+<h1> Target Data: </h1>
 
 <%= form_with url: targets_path, method: :post do |form|%>
     <div class="mb-3">
@@ -6,15 +6,32 @@
     <%= form.collection_select :client_id, Client.all, :id, :name, prompt:"Select One", class: "form-control" %>
     </div>
     <div class="mb-3">
+    <%= form.label :title, "Taret Title:" %>
+    <%= form.text_field :Target, class: "form-control" %>
+    </div>
+    <div class="mb-3">
     <%= form.label :description, "Description:" %>
     <%= form.text_field :description, class: "form-control" %>
     </div>
     <div class="mb-3">
+    <%= form.label :protocols, "Protocols:" %>
+    <%= form.text_field :protocols, class: "form-control" %>
+    </div>
+    <div class="mb-3">
     <%= form.label :score_correct, "Correct" %>
     <%= form.check_box :score_correct, class: "form-control" %>
-    </div><div class="mb-3">
-    <%= form.label :score_incorrect, "This client is eligible for insurance coverage." %>
-    <%= form.check_box :insurance, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :score_incorrect, "Incorrect" %>
+    <%= form.check_box :score_incorrect, class: "form-control" %>
+    </div>
+    
+    <div class="mb-3">
+    <%= form.label :trials, "Trails Ran:" %>
+    <%= form.number_field :trials, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.submit "Submit Data", class: "btn btn-primary" %>
     </div>
 
 <% end %>
