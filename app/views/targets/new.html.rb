7e27afd35447f68597eba3e5f95097ff2d0029ad
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-05-02 21:30:26 -0500

    Target page update

--- app/views/targets/new.html.rb
+++ app/views/targets/new.html.rb
@@ -5,5 +5,16 @@
     <%= form.label :client_id, "Client First and Last name:" %>
     <%= form.collection_select :client_id, Client.all, :id, :name, prompt:"Select One", class: "form-control" %>
     </div>
+    <div class="mb-3">
+    <%= form.label :description, "Description:" %>
+    <%= form.text_field :description, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :score_correct, "Correct" %>
+    <%= form.check_box :score_correct, class: "form-control" %>
+    </div><div class="mb-3">
+    <%= form.label :score_incorrect, "This client is eligible for insurance coverage." %>
+    <%= form.check_box :insurance, class: "form-control" %>
+    </div>
 
 <% end %>
