b032f86a03a0a7c62978ac2ece4a3faea473fc18
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-05-02 21:56:36 -0500

    new target form

--- app/views/targets/new.html.rb
+++ app/views/targets/new.html.rb
@@ -210,6 +210,108 @@
     <%= form.number_field :trials, class: "form-control" %>
     </div>
     <div class="mb-3">
+    <%= form.label :title, "Taret Title:" %>
+    <%= form.text_field :Target, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :description, "Description:" %>
+    <%= form.text_field :description, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :protocols, "Protocols:" %>
+    <%= form.text_field :protocols, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :score_correct, "Correct" %>
+    <%= form.check_box :score_correct, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :score_incorrect, "Incorrect" %>
+    <%= form.check_box :score_incorrect, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :verbal_prompt, "Verbal Prompt" %>
+    <%= form.check_box :verbal_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :partial_verbal_prompt, "Partial Verbal Prompt" %>
+    <%= form.check_box :partial_verbal_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :full_physical_prompt, "Full Physical Prompt" %>
+    <%= form.check_box :full_physical_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :partial_physical_prompt, "Partial Physical Prompt" %>
+    <%= form.check_box :partial_physical_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :gesture_prompt, "Gesture Prompt" %>
+    <%= form.check_box :gesture_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :visual_prompt, "Visual Prompt" %>
+    <%= form.check_box :visual_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :modeling_prompt, "Modeling Prompt" %>
+    <%= form.check_box :modeling_prompt, class: "form-control" %>
+    </div>
+    <%= form.label :trials, "Trails Ran:" %>
+    <%= form.number_field :trials, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :title, "Taret Title:" %>
+    <%= form.text_field :Target, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :description, "Description:" %>
+    <%= form.text_field :description, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :protocols, "Protocols:" %>
+    <%= form.text_field :protocols, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :score_correct, "Correct" %>
+    <%= form.check_box :score_correct, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :score_incorrect, "Incorrect" %>
+    <%= form.check_box :score_incorrect, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :verbal_prompt, "Verbal Prompt" %>
+    <%= form.check_box :verbal_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :partial_verbal_prompt, "Partial Verbal Prompt" %>
+    <%= form.check_box :partial_verbal_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :full_physical_prompt, "Full Physical Prompt" %>
+    <%= form.check_box :full_physical_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :partial_physical_prompt, "Partial Physical Prompt" %>
+    <%= form.check_box :partial_physical_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :gesture_prompt, "Gesture Prompt" %>
+    <%= form.check_box :gesture_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :visual_prompt, "Visual Prompt" %>
+    <%= form.check_box :visual_prompt, class: "form-control" %>
+    </div>
+    <div class="mb-3">
+    <%= form.label :modeling_prompt, "Modeling Prompt" %>
+    <%= form.check_box :modeling_prompt, class: "form-control" %>
+    </div>
+    <%= form.label :trials, "Trails Ran:" %>
+    <%= form.number_field :trials, class: "form-control" %>
+    </div>
+    <div class="mb-3">
     <%= form.submit "Submit Data", class: "btn btn-primary" %>
     </div>
 
