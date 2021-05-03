f55470ecee0a9ab6b06028ec55280fa8ea4dc481
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-05-03 17:33:54 -0500

    main image add to employee and client

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,7 @@ class Client < ApplicationRecord
     has_many :behaviors
     has_many :appointments
     has_many :employees, through: :appointments
+    has_one_attached :main_image
 
     has_secure_password
 
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,7 @@ class Employee < ApplicationRecord
     has_many :notes
     has_many :appointments
     has_many :clients, through: :appointments
+    has_one_attached :main_image
 
     has_secure_password
 end
