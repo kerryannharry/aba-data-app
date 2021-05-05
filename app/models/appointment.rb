f8363768353959b73a348d5aa6cc4fd8bdd68b1f
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-05-05 16:34:44 -0500

    scope method in appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,4 +4,11 @@ class Appointment < ApplicationRecord
     has_one :note
 
     validates :start_time, :duration, :client_id, :employee_id, presence:true
+
+    scope :scheduled ->{ where(STATUS[:scheduled]) }
+    scope :draft ->{ where(STATUS[:draft]) }
+    STATUS = {
+        scheduled: "Scheduled"
+        draft: "Draft"
+    }
 end
